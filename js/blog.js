6678c24f871e160b5a62d1bcf85b7b60b788d55f
Ahmetcan Aksu <ahmetcanco@gmail.com>
2021-12-03 23:30:26 +0300

    Fix blog issues

--- js/blog.js
+++ js/blog.js
@@ -1,9 +1,9 @@
 var targeted = new URLSearchParams(location.search).get("page");
-
 if (targeted == null) {
     fetch('https://raw.githubusercontent.com/behemehal/EllieBlog/main/blog.json')
         .then(response => response.json())
         .then(data => {
+            document.querySelector('#rm').style.display = 'block';
             let light = false;
             for (var i = 0; i < data.length; i++) {
                 var post = data[i];
@@ -88,65 +88,71 @@ if (targeted == null) {
     fetch('https://raw.githubusercontent.com/behemehal/EllieBlog/main/blogs/' + targeted)
         .then(response => response.text())
         .then(data => {
-            var lineEnding = whichLineEnding(data);
-            var lines = data.split(lineEnding);
-            var conf = lines.slice(1, 7);
-            var md = lines.slice(8);
-            var parsedConf = convertToObject(conf.map(x => parseKeyValue(x)));
+            if (data == "404: Not Found") {
+                location = "/404.html";
+            } else {
+                var lineEnding = whichLineEnding(data);
+                var lines = data.split(lineEnding);
+                var conf = lines.slice(1, 7);
+                var md = lines.slice(8);
+                var parsedConf = convertToObject(conf.map(x => parseKeyValue(x)));
 
-            marked.setOptions({
-                renderer: new marked.Renderer(),
-                highlight: function (code, lang) {
-                    const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-                    return hljs.highlight(code, {
-                        language
-                    }).value;
-                },
-                langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
-                pedantic: false,
-                gfm: true,
-                breaks: false,
-                sanitize: false,
-                smartLists: true,
-                smartypants: false,
-                xhtml: false
-            });
+                marked.setOptions({
+                    renderer: new marked.Renderer(),
+                    highlight: function (code, lang) {
+                        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+                        return hljs.highlight(code, {
+                            language
+                        }).value;
+                    },
+                    langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
+                    pedantic: false,
+                    gfm: true,
+                    breaks: false,
+                    sanitize: false,
+                    smartLists: true,
+                    smartypants: false,
+                    xhtml: false
+                });
 
-            var script = document.createElement("script");
-            script.setAttribute("src", 'https://giscus.app/client.js');
-            script.setAttribute("data-repo", "behemehal/ellieblog");
-            script.setAttribute("data-repo-id", "R_kgDOGdv8Ow");
-            script.setAttribute("data-category", "Comments");
-            script.setAttribute("data-category-id", "DIC_kwDOGdv8O84CAHDn");
-            script.setAttribute("data-mapping", "specific");
-            script.setAttribute("data-term", parsedConf.title);
-            script.setAttribute("data-reactions-enabled", "1");
-            script.setAttribute("data-emit-metadata", "0");
-            script.setAttribute("data-theme", "https://ellie.behemehal.net/css/gis_theme.css");
-            script.setAttribute("data-lang", "en");
-            script.setAttribute("crossorigin", "anonymous");
+                var script = document.createElement("script");
+                script.setAttribute("src", 'https://giscus.app/client.js');
+                script.setAttribute("data-repo", "behemehal/ellieblog");
+                script.setAttribute("data-repo-id", "R_kgDOGdv8Ow");
+                script.setAttribute("data-category", "Comments");
+                script.setAttribute("data-category-id", "DIC_kwDOGdv8O84CAHDn");
+                script.setAttribute("data-mapping", "specific");
+                script.setAttribute("data-term", parsedConf.title);
+                script.setAttribute("data-reactions-enabled", "1");
+                script.setAttribute("data-emit-metadata", "0");
+                script.setAttribute("data-theme", "https://ellie.behemehal.net/css/gis_theme.css");
+                script.setAttribute("data-lang", "en");
+                script.setAttribute("crossorigin", "anonymous");
 
-            var owner = document.createElement("div");
-            owner.style = "text-align: right;";
-            owner.innerHTML = `<a style="text-align: right;" href="https://github.com/${parsedConf.publisher.toString().replace("@", "")}">Created by ${parsedConf.publisher}</a>
-            <p style="text-align: right;">Created at ${moment(parsedConf.date).format("MM.DD.YYYY HH:mm")}</p>`
+                var owner = document.createElement("div");
+                owner.style = "text-align: right;";
+                owner.innerHTML = `<a style="text-align: right;" href="https://github.com/${parsedConf.publisher.toString().replace("@", "")}">Created by ${parsedConf.publisher}</a>
+                <p style="text-align: right;">Created at ${moment(parsedConf.date, "MM.DD.YYYY HH:mm").format("MM.DD.YYYY HH:mm")}</p>`;
 
-            if (conf.updated_by) {
-                owner.innerHTML = `<br><a style="text-align: right;" href="https://github.com/${parsedConf.updated_by.toString().replace("@", "")}">Updated by: ${parsedConf.updated_by}</a>
-                <p style="text-align: right;">Created at ${moment(parsedConf.updated_at).format("MM.DD.YYYY HH:mm")}</p>`
+                console.log(parsedConf)
+                if (conf.updated_by) {
+                    owner.innerHTML = `<br><a style="text-align: right;" href="https://github.com/${parsedConf.updated_by.toString().replace("@", "")}">Updated by: ${parsedConf.updated_by}</a>
+                    <p style="text-align: right;">Created at ${moment(parsedConf.updated_at, "MM.DD.YYYY HH:mm").format("MM.DD.YYYY HH:mm")}</p>`;
+                }
+
+                var div = document.createElement('div');
+                div.style.padding = "3rem";
+                div.innerHTML += marked.parse(md.join(lineEnding));
+                var gis = document.createElement('div');
+                gis.className = "giscus";
+                div.appendChild(owner);
+                div.appendChild(gis)
+                document.body.appendChild(script);
+                document.querySelector('.blogs').appendChild(div)
+                document.querySelector('.loader').style.display = 'none';
+                document.querySelector('.blogs').style.display = 'block';
             }
-
-            var div = document.createElement('div');
-            div.style.padding = "3rem";
-            div.innerHTML += marked.parse(md.join(lineEnding));
-            var gis = document.createElement('div');
-            gis.className = "giscus";
-            div.appendChild(owner);
-            div.appendChild(gis)
-            document.body.appendChild(script);
-            document.querySelector('.blogs').appendChild(div)
-            document.querySelector('#rm').style.display = 'none';
-            document.querySelector('.loader').style.display = 'none';
-            document.querySelector('.blogs').style.display = 'block';
+        }).catch(function (err) {
+            location = "/404.html";
         });
 }
